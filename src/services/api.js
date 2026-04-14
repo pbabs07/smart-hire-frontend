d8404b9f1303a179fb588264ea68b4e449d6b68a
Olaposi BABATUNDE <53888201+pbabs07@users.noreply.github.com>
2026-04-15 00:44:44 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://job-application-tracker-saas.onrender.com/api,
+  baseURL: "https://job-application-tracker-saas.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
